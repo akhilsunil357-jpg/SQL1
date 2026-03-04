fdf1a2148aef03c44043a1a475bdcdffe2b7c5a6
Akhil sunil <akhilsunil357@gmail.com>
2026-03-04 23:49:57 +0530

    WORK8.1

---
+++ work8.1.sql
@@ -0,0 +1,30 @@
+create DATABASE BookstoreDB;
+
+use BOOKSTOREDB;
+
+
+CREATE TABLE authors (
+    author_id INT PRIMARY KEY,
+    name VARCHAR(100),
+    country VARCHAR(100)
+);
+
+CREATE TABLE books (
+    book_id INT PRIMARY KEY,
+    title VARCHAR(200),
+    price DECIMAL(10,2),
+    author_id INT,
+    FOREIGN KEY (author_id) REFERENCES authors(author_id)
+);
+
+ALTER TABLE books
+ADD published_year INT;
+
+
+SELECT * FROM BOOKS;
+
+
+TRUNCATE TABLE books;
+
+
+DROP DATABASE BookStoreDB;
